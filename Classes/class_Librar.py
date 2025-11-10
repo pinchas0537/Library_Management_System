c38f2c350254ed4e77d5872ec1035898e099807d
David Kubayof <davidkubayof@gmail.com>
2025-11-10 10:07:56 +0200

    first commit of class_Librar.py file

---
+++ Classes/class_Librar.py
@@ -0,0 +1,18 @@
+class Library:
+    def __init__(self):
+        self.list_of_books = []
+        self.list_of_user = []
+
+
+    def add_book(book):
+
+
+    def add_user(user):
+
+    def borrow_book(user_id, book_isbn):
+    
+    def return_book(user_id, book_isbn):
+        
+    def list_available_books():
+    
+    def search_book(title or author):
