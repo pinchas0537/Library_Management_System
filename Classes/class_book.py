6481232195cf5c9e5716b624c2aa2d0c6c3b46b6
David Kubayof <davidkubayof@gmail.com>
2025-11-10 16:38:28 +0200

    del

--- Classes/class_book.py
+++
@@ -1,9 +0,0 @@
-class Book:
-    def __init__(self, title, author, ISBN):
-        self.title = title
-        self.author = author
-        self.ISBN = ISBN
-        self.is_available = True 
-
-    def __str__(self):
-        return f"title: {self.title}  author: {self.author} is_available: {self.is_available}"
