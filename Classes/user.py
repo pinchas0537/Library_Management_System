3a1ca80c97c4ec535b6bfd88bf321c02fb23aa70
Pinchas Lifshitz <pnl8480537@gmail.com>
2025-11-10 12:37:12 +0200

    finsh user.py file

--- Classes/user.py
+++ Classes/user.py
@@ -1,8 +1,9 @@
+from uuid import uuid4
 class User:
-    def __init__(self,name, id, borrowed_books: list[Book]):
+    def __init__(self,name):
         self.name = name
-        self.id = id
-        self.borrowed_books = borrowed_books
+        self.id = str(uuid4())
+        self.borrowed_books = list[Book]
     def __str__(self):
         return f"name: {self.name} id: {self.id} books: {self.borrowed_books}"
         
