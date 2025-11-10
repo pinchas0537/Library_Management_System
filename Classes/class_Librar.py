5d6c4988c7b2a1400ad16dd68f5600d00983cfef
David Kubayof <davidkubayof@gmail.com>
2025-11-10 12:44:19 +0200

    add

--- Classes/class_Librar.py
+++ Classes/class_Librar.py
@@ -3,16 +3,38 @@ class Library:
         self.list_of_books = []
         self.list_of_user = []
 
-
-    def add_book(book):
-
-
-    def add_user(user):
-
-    def borrow_book(user_id, book_isbn):
+    def add_book(self , book):
+        self.book = book
+        self.list_of_books.append([book])
+        return self.list_of_books
     
-    def return_book(user_id, book_isbn):
+    def add_user(self ,user):
+        self.user = user
+        self.list_of_user.append([self.user])
+        return self.list_of_user
+
+    def borrow_book(self , user_id, book_isbn):#take
+        self.user_id = user_id
+        self.book_isbn = book_isbn
+        for book in self.list_of_books:
+            if book_isbn == self.book.ISBN:
+                self.user.borrowed_books.append(book)
+                self.book.is_available = False
+           
         
-    def list_available_books():
+
+
+
+
+    # def return_book(user_id, book_isbn):#bake
+    #     pass
     
-    def search_book(title or author):
+       
+    # def list_available_books(self):
+    #     for book in self.list_of_books:
+            
+
+    # def search_book(self , title , author):
+    #     for book in self.list_of_books:
+    #         if title or author in book:
+    #             return book
--- Classes/user.py
+++ Classes/user.py
@@ -1,8 +1,8 @@
 class User:
-    def __init__(self,name, id, borrowed_books: list[Book]):
+    def __init__(self,name, id):
         self.name = name
         self.id = id
-        self.borrowed_books = borrowed_books
+        self.borrowed_books = []
+
     def __str__(self):
         return f"name: {self.name} id: {self.id} books: {self.borrowed_books}"
-        
