e757916f314d1fa86fc7d9ae511484d4d46a1368
David Kubayof <davidkubayof@gmail.com>
2025-11-10 14:56:14 +0200

    finsh user.py file

--- Classes/user.py
+++ Classes/user.py
@@ -1,9 +1,10 @@
 from uuid import uuid4
+
 class User:
 
     def __init__(self,name):
         self.name = name
         self.id = str(uuid4())
-        self.borrowed_books = list[Book]
+        self.borrowed_books = list()
     def __str__(self):
         return f"name: {self.name} id: {self.id} books: {self.borrowed_books}"
