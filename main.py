45bc83dbac128969e960679f8b5455411cf4e6e5
David Kubayof <davidkubayof@gmail.com>
2025-11-10 14:59:44 +0200

    addedmain.py file

---
+++ main.py
@@ -0,0 +1,23 @@
+from Classes.class_book import Book
+from Classes.class_Librar import Library
+from Classes.user import User
+
+if __name__ == "__main__":    
+    book1 = Book("WAR","DAVID",1)
+    book2 = Book("LOVE","DAN",2)
+    
+    user1 = User("MOSHA")
+    user2 = User("NOA")
+    
+    libary = Library()
+
+    libary.add_book(book1.__str__())
+    libary.add_book(book2.__str__())
+
+    libary.add_user(user1.__str__())
+    libary.add_user(user2.__str__())
+
+    libary.borrow_book(user1.id,book1.ISBN)
+    
+    print(book1.is_available)
+    print(book2.is_available)
